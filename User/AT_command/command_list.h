1ec4a324b8cf354a5812f78f4431676d2451d1db
ZHH-start <84170546+ZHH-start@users.noreply.github.com>
2024-08-07 00:46:45 +0800

    添加了初始化完成提示词

--- User/AT_command/command_list.h
+++ User/AT_command/command_list.h
@@ -9,7 +9,7 @@
 #define COMMAND_3 "CM+isr_read=1s\r\n" // 一秒回传一次 指令
 #define COMMAND_4 "CM+isr_read=2s\r\n" // 两秒回传一次 指令
 #define COMMAND_5 "CM+stop_isr\r\n"
-// #define COMMAND_6 "1234567"
+// #define COMMAND_6 "CM+uart_config="
 // #define COMMAND_7 "1234567"
 // #define COMMAND_8 "1234567"
 // #define COMMAND_9 "1234567"
