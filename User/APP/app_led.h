adc0360e7611014ae0a11018b50e2051f2f104e0
ZHH-start <84170546+ZHH-start@users.noreply.github.com>
2024-09-17 19:51:42 +0800

    建立了简单框架

---
+++ User/APP/app_led.h
@@ -0,0 +1,8 @@
+#ifndef		__APP_LED_H
+#define		__APP_LED_H
+
+#include "main.h"
+
+
+
+#endif
--- User/main.c
+++ User/main.c
@@ -11,18 +11,19 @@ int main(void)
     HAL_Init(); /* HAL库初始化 */
 
     Bsp_Led_Init();                 /* LED 初始化 */
-    DEBUG_USART_Config(115200);       // UART初始化 A6-TX A7-RX
-    Rx_Data_Buf_Init(&Rx_Data_Buf); /* USART 接收数据缓冲区初始化 */
+    // DEBUG_USART_Config(115200);       // UART初始化 A6-TX A7-RX
+    // Rx_Data_Buf_Init(&Rx_Data_Buf); /* USART 接收数据缓冲区初始化 */
 
-    PWM_LED(LED_ON);
+    // PWM_LED(LED_ON);
 
-    sensirion_i2c_hal_init();
-    sht3x_init(SHT30_I2C_ADDR_44);
+    // sensirion_i2c_hal_init();
+    // sht3x_init(SHT30_I2C_ADDR_44);
 
     printf("MPT-SHT30-WIRE01 ready\r\n");
 
     while (1) {
-        command_run();
+        // command_run();
+        
     }
 }
 
