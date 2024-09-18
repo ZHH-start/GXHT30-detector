d0d388a3e998dc23a9bc7591baaa883f375ad0f0
ZHH-start <84170546+ZHH-start@users.noreply.github.com>
2024-09-18 20:53:08 +0800

    发现问题，iic初始化频率失效。

--- User/APP/app_sht_read.h
+++ User/APP/app_sht_read.h
@@ -5,8 +5,8 @@
 #include "app.h"
 
 #define I2C_ADDRESS      0x78               /* Local address 0xA0 */
-#define I2C_SPEEDCLOCK   40000             /* Communication speed 100K */
-#define I2C_DUTYCYCLE    I2C_DUTYCYCLE_16_9 /* Duty cycle */
+#define I2C_SPEEDCLOCK   4000             /* Communication speed 100K */
+#define I2C_DUTYCYCLE    I2C_DUTYCYCLE_2 /* Duty cycle */
 
 void app_sht_read_init();
 void app_sht_read();
